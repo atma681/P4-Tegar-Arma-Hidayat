998c4367456a43c17ec280e32f66482c69a26388
atma681 <64574180+atma681@users.noreply.github.com>
2020-05-11 15:38:01 +0800

    Create JOBSHEET 1 C.cpp
    Membuat Program Array Satu Dimensi Yg Menampilkan NIM Masing-Masing Mahasiswa

---
+++ JOBSHEET 1 C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<" Tegar Atma Hidayat-F1B019137-KLP 26\n\n";
+int nim[5]= {7,13,9,9,8};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+}
