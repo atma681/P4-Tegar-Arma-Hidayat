18a15d6773734c36f3b6ae32252f342e26297b6d
atma681 <64574180+atma681@users.noreply.github.com>
2020-05-11 01:28:53 -0700

    Create JOBSHEET 5 C.cpp
    Membuat Program Perkalian Matrix Dua Dimensi Dengan Menggunakan NIM Dua Orang Mahasiswa

---
+++ JOBSHEET 5 C.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+main ()
+{
+	cout<<"Tegar Atma Hidayat (F1B019137)-kel.26\n\n";
+	int nim1[2][2];
+	int nim2[2][2];
+	int jml [2][2];
+	int x, y;
+	cout << "=====PERKALIAN MATRIX=====\n";
+	for (x = 0; x < 2; x++){
+	for (y = 0; y < 2; y++){
+		cout << "*Masukkan index matriks pertama* " << x << "," << y << " = ";
+			cin >> nim1[x][y];
+		}
+	}
+	cout << endl;
+	for (x = 0; x < 2; x++){
+	for (y = 0; y < 2; y++){
+		cout << "*Masukkan index matriks kedua* " << x << "," << y << " = ";
+			cin >> nim2[x][y];
+		}
+	}
+	cout << endl;
+	for (x = 0; x < 2; x++){
+	for (y = 0; y < 2; y++){
+		jml [x][y] = nim1[x][y]*nim2[x][y];
+			cout << jml [x][y] <<" ";
+		}
+		cout << endl;
+    return 0;
+	}
+}
