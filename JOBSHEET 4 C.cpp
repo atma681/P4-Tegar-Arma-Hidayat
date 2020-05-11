64b274c1a43c0c817450d3eb627caeca2ddae0f2
atma681 <64574180+atma681@users.noreply.github.com>
2020-05-11 01:33:24 -0700

    Update JOBSHEET 4 C.cpp
    Membuat Program Pengurangan Matrix Menggunakan NIM Dua Orang Mahasiswa

--- JOBSHEET 4 C.cpp
+++ JOBSHEET 4 C.cpp
@@ -2,29 +2,31 @@
 #include <cstdlib>
 using namespace std;
 
-main (){
+main ()
+{
+	cout<<"Tegar Atma Hidayat (F1B019137)-Kel.26\n\n";
 	int nim1[2][2];
 	int nim2[2][2];
 	int jml [2][2];
 	int x, y;
 	cout << "PENGURANGAN MATRIX\n";
 	for (x = 0; x < 2; x++){
-		for (y = 0; y < 2; y++){
-			cout << "Masukkan index " << x << "," << y << " = ";
+	for (y = 0; y < 2; y++){
+		cout << "Masukkan index " << x << "," << y << " = ";
 			cin >> nim1[x][y];
 		}
 	}
 	cout << endl;
 	for (x = 0; x < 2; x++){
-		for (y = 0; y < 2; y++){
-			cout << "Masukkan index " << x << "," << y << " = ";
+	for (y = 0; y < 2; y++){
+		cout << "Masukkan index " << x << "," << y << " = ";
 			cin >> nim2[x][y];
 		}
 	}
 	cout << endl;
 	for (x = 0; x < 2; x++){
-		for (y = 0; y < 2; y++){
-			jml [x][y] = nim1[x][y]-nim2[x][y];
+	for (y = 0; y < 2; y++){
+		jml [x][y] = nim1[x][y]-nim2[x][y];
 			cout << jml [x][y] <<" ";
 		}
 		cout << endl;
