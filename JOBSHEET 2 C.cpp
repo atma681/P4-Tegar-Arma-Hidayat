91381e361c0e97eb6d0ad089580dde1124a611ce
atma681 <64574180+atma681@users.noreply.github.com>
2020-05-11 00:57:23 -0700

    Create JOBSHEET 2 C.cpp
    MEmbuat Program Mengisi Dan Menampilkan Elemen Array Dengan Index Sesuai NIM Terakhir Setiap Mahasiswa

---
+++ JOBSHEET 2 C.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int x[5];
+	cout<<"Tegar Atma Hidayat F1B019137 KLP 26";
+	cout<<"\nMasukkan NIM 1:";
+	cin>>x[0];
+	cout<<"\nMasukkan NIM 2:";
+	cin>>x[1];
+	cout<<"\nMasukkan NIM 3:";
+	cin>>x[2];
+	cout<<"\nMasukkan NIM 4:";
+	cin>>x[3];
+	cout<<"\nMasukkan NIM 5:";
+	cin>>x[4];
+	cout<<"\n";
+	cout<<"NIM KE-1 = "<<x[0];
+	cout<<"\nNIM KE-2 = "<<x[1];
+	cout<<"\nNIM KE-3 = "<<x[2];
+	cout<<"\nNIM KE-4 = "<<x[3];
+	cout<<"\nNIM KE-5 = "<<x[4];
+	
+	return 0;
+	
+}
+
+
+
